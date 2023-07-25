76688bd2717b491e97fa35fd1f85ddc8807bfb61
soulaichhi <chhibou.soulaimane@gmail.com>
2023-07-25 13:58:55 +0100

    Adding Store-Dev-tools

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import {RouterModule, Routes} from "@angular/router";
+import {NgModule} from "@angular/core";
+import {HomeComponent} from "./home/home.component";
+import {CounterComponent} from "./counter/counter/counter.component";
+import {PostsListComponent} from "./posts/posts-list/posts-list.component";
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'counter', component: CounterComponent},
+  {path: 'posts', component: PostsListComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import {NgModule} from '@angular/core';
+import {isDevMode, NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
@@ -9,6 +9,11 @@ import {StoreModule} from "@ngrx/store";
 import {counterReducer} from "./counter/state/counter.reducer";
 import {CustomCounterInputComponent} from './counter/custom-counter-input/custom-counter-input.component';
 import {FormsModule} from "@angular/forms";
+import {HomeComponent} from './home/home.component';
+import {AppRoutingModule} from "./app-routing.module";
+import {HeaderComponent} from './shared/components/header/header.component';
+import {PostsListComponent} from './posts/posts-list/posts-list.component';
+import {StoreDevtoolsModule} from "@ngrx/store-devtools";
 
 @NgModule({
   declarations: [
@@ -16,11 +21,22 @@ import {FormsModule} from "@angular/forms";
     CounterComponent,
     CounterOutputComponent,
     CounterButtonsComponent,
-    CustomCounterInputComponent
+    CustomCounterInputComponent,
+    HomeComponent,
+    HeaderComponent,
+    PostsListComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     StoreModule.forRoot({counter: counterReducer}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: !isDevMode(), // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+      trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
+      traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
+    }),
     FormsModule
   ],
   providers: [],
