0b7c6877fe3f05adb1fd2d4c4c87f0597f9296dc
soulaichhi <chhibou.soulaimane@gmail.com>
2023-07-27 17:48:21 +0100

    Design Auth Login Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,8 @@ import {HomeComponent} from "./home/home.component";
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'counter', loadChildren: () => import('./counter/counter.module').then(m => m.CounterModule)},
-  {path: 'posts', loadChildren: () => import('./posts/posts.module').then(m => m.PostsModule)}
+  {path: 'posts', loadChildren: () => import('./posts/posts.module').then(m => m.PostsModule)},
+  {path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)}
 ];
 
 @NgModule({
